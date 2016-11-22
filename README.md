dee235a9c8fbc0c531f383b7ef45b9a1a09a551e
Luc Phuong Nghia <nghialp@tocdep.vn>
2016-11-22 04:37:12 +0000

    create test for listing artilec and no artile listed

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Git for version control
 - add files and new changes to track
 - commit the changes or discard it as well
 - save code to online repositories 
+
+Listing article feature test
+- create branch
+- create 2 articles to display
+- list the two articles
+- expect both title and body of the two articles to display
---
+++ spec/features/listing_article_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.feature "Listing articles" do
+  before do
+    @article1 = Article.create(title: "first article", body: "This is the first article")
+    @article2 = Article.create(title: "Second article", body: "This is the second article")
+  end
+  
+  scenario "A user lists all articles" do
+    visit "/"
+    
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+  
+  scenario "A user has no article to list" do
+    Article.delete_all
+    visit "/"
+    
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    
+    within ("h1#no-articles") do
+      expect(page).to have_content("No article created")
+    end
+  end
+end
