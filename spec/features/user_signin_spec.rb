eea045dfb22d3da4c2b186ba8b6a72aef382b483
Luc Phuong Nghia <nghialp@tocdep.vn>
2016-11-23 05:50:31 +0000

    Setup user signin and styling

---
+++ spec/features/user_signin_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "User sign in" do
+  before do
+    @john = User.create!(email: "john@gmail.com", password: "password")
+  end
+  
+  scenario "User signin with valid credentials" do
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
+    click_button "Log in"
+    
+    expect(page).to have_content("Signed in successfully")
+    expect(page).to have_content("Signed in as #{@john.email}")
+    expect(page).not_to have_link("Sign in")
+    expect(page).not_to have_link("Sign up")
+  end
+end
